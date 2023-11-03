2d67af4619a14b9a9f987d2b84d9937e681fa101
Saagar <saagarpatel2601@gmail.com>
2023-11-03 14:03:08 -0500

    add rise and set times and more scaffold

---
+++ main.py
@@ -0,0 +1,45 @@
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix = '!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command(name='hello')
+async def hello(ctx):
+    if ctx.author == bot.user:
+        return
+    await ctx.send('Hello once!')
+
+
+"""
+Will output the daily summary objects and phenomena to the channel
+
+--- Params
+
+None
+"""
+@bot.command(name="Daily Summary")
+async def daily_summary(ctx):
+    pass
+
+"""
+Will output the coordinates and relative direction of the 
+"""
+@bot.command(name="Locate")
+async def locate(ctx, arg):
+    pass
+
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await bot.close()
+    print("BOT HAS SHUTDOWN")
+
+bot.run('MTE0ODAxNzI3Nzk0NjU2MDUyMg.GWqVrr.pBGIWQVaiu4WdNgb_tdHmZqC9lUz744GvZLQyc')
---
+++ sky.py
@@ -0,0 +1,6 @@
+import datetime as dt
+import skyfield as sky
+from skyfield import almanac
+from skyfield.api import N, W, wgs84, load
+from pytz import timezone
+
